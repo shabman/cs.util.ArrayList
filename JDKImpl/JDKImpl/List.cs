74ef64d5ea69fafb850d03626f31f73cc4c6b982
shabman <59781727+shabman@users.noreply.github.com>
2022-03-27 01:12:13 +0400

    Add files via upload

---
+++ JDKImpl/JDKImpl/List.cs
@@ -0,0 +1,96 @@
+﻿namespace JDKImpl
+{
+    public interface IList<T>
+    {
+        void Add(T args);
+
+        T Get(int index);
+
+        T Get(T args);
+
+        void Remove(int index);
+
+        void Remove(T args);
+    }
+    public class ArrayList<T> : IList<T>
+    {
+
+        private T[] buffer = new T[10000];
+        private T[] backup;
+
+        private int index = 0;
+
+        public ArrayList() { }
+
+        public void Add(T args)
+        {
+            if (index == buffer.Length - 1)
+            {
+                Backup(buffer, buffer.Length * 2);
+            }
+            if (backup != null && index == backup.Length - 1)
+            {
+                Reset(backup, backup.Length * 2);
+            }
+            buffer[index] = args;
+            index++;
+        }
+
+        public T Get(int index)
+        {
+            return buffer[index];
+        }
+
+        public T Get(T args)
+        {
+            foreach (object element in buffer)
+            {
+                if (element == (object) args)
+                {
+                    return (T) element;
+                }
+            }
+            return default;
+        }
+
+        public void Remove(int index)
+        {
+            buffer[index] = default;
+        }
+
+        public void Remove(T args)
+        {
+            int inc = 0;
+            foreach (object element in buffer)
+            {
+                if (element == (object) args)
+                {
+                    buffer[index] = default;
+                    break;
+                }
+                inc++;
+            }
+            index--;
+        }
+
+        private void Backup(T[] old, int size)
+        {
+            backup = new T[size];
+            for (int i = 0; i < old.Length - 1; i++)
+            {
+                backup[i] = buffer[i];
+                buffer[i] = default;
+            }
+        }
+
+        private void Reset(T[] current, int size)
+        {
+            buffer = new T[size];
+            for (int i = 0; i < current.Length - 1; i++)
+            {
+                buffer[i] = backup[i];
+                backup[i] = default;
+            }
+        }
+    }
+}
---
+++ JDKImpl/JDKImpl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JDKImpl
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IList<string> list = new ArrayList<string>();
+
+            list.Add("hi");
+            list.Add("Super");
+            list.Add("Cool");
+            list.Add("Test");
+
+            Console.WriteLine(list.Get(0));
+            Console.WriteLine(list.Get("hi"));
+        }
+    }
+}
